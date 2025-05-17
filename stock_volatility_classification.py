64f5f7bd43bb7aa566d4b2a844cdc33ae09a2696
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-05-17 13:28:11 +0100

    I don't know how to solve this

--- stock_volatility_classification.py
+++ stock_volatility_classification.py
@@ -4,13 +4,17 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.model_selection import TimeSeriesSplit
 
 df = pd.read_csv('/Users/ohavryleshko/Documents/GitHub/Kaggle datasets/15 Years Stock Data of NVDA AAPL MSFT GOOGL and AMZN.csv')
 print(df.columns)
 
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
 
 def stock_iteration_gb(df, stock):
+    print('Initialising the function...',)
     df = df.copy()
     df[f'Volatility_{stock}'] = df[f'High_{stock}'] - df[f'Low_{stock}']
     median = df[f'Volatility_{stock}'].median()
@@ -42,10 +46,53 @@ def stock_iteration_gb(df, stock):
     gb = GradientBoostingClassifier()
     gb.fit(X_train, y_train)
 
+    train_accuracy = gb.score(X_train, y_train)
+    print(f'Accuracy score for training dataset: {train_accuracy}')
+
+    test_accuracy = gb.score(X_test, y_test)
+    print(f'Accuracy for test dataset: {test_accuracy}')
+
     #evaluation
     y_pred = gb.predict(X_test)
     print(accuracy_score(y_pred, y_test))
     print(confusion_matrix(y_pred, y_test))
+    
+    param_grid = {
+        'n_estimators': [100, 200],
+        'learning_rate': [0.1, 0.05],
+        'max_depth': [2, 3],
+    }
+
+    gs = GridSearchCV(gb, param_grid, cv=5, scoring='f1')
+    gs.fit(X_train, y_train)
+
+    print(f'Best parameters are: {gs.best_params_}')
+    best_model = gs.best_estimator_
+    print(best_model)
+
+    y_pred = best_model.predict(X_test)
+    print(accuracy_score(y_pred, y_test))
+    print(confusion_matrix(y_pred, y_test))
+
+    from sklearn.metrics import classification_report
+    print(classification_report(y_pred, y_test))
+
+    tscv = TimeSeriesSplit(n_splits=5)
+    for train_index, test_index in tscv.split(df):
+        print(train_index, test_index)
+
+    from sklearn.model_selection import learning_curve
+    train_size, train_score, test_score = learning_curve(gb, X, y, cv=5)
+    train_mean = train_score.mean(axis=1)
+    test_mean = test_score.mean(axis=1)
+
+    plt.plot(train_size, train_mean, label='Training acc')
+    plt.plot(train_size, test_mean, label='Test acc')
+    plt.xlabel('Training size')
+    plt.ylabel('Acc')
+    plt.title('learning curve')
+    plt.legend()
+    plt.show()
 
 for stock in ['AAPL', 'AMZN', 'MSFT', 'NVDA', 'GOOGL']:
     stock_iteration_gb(df, stock)
